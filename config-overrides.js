3425c3127691e9353266161fac916b8eed83f0a6
OlehSyzonov <oleg.sizonov@gmail.com>
2024-12-04 00:03:37 +0000

    npm install crypto-browserify

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+// config-overrides.js
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    crypto: require.resolve('crypto-browserify')
+  };
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ];
+  return config;
+};
